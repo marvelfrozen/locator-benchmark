aabf9dd902905d51efb75ab455200b9bbc63df91
marvelfrozen <marvelfrozen@gmail.com>
2023-10-24 09:15:54 +0700

    refactor the code for easier configuration

--- src/test/kotlin/SeleniumBenchmark.kt
+++ src/test/kotlin/SeleniumBenchmark.kt
@@ -1,12 +1,44 @@
 import io.github.bonigarcia.wdm.WebDriverManager
 import org.junit.jupiter.api.Test
-import org.openqa.selenium.By
+import org.openqa.selenium.By.*
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import java.io.File
 import java.time.Instant
 
 class SeleniumBenchmark {
+  data class Locator(val by: String, val expression: String)
+
+  private val commandsByName = listOf(::xpath, ::id, ::name, ::cssSelector).associateBy { it.name }
+
+  private val target = "https://www.youtube.com/"
+  private val numberOfCycle = 100
+  private val testLocators =
+      listOf(
+          Locator("xpath", "//*[@id=\"search\"]"),
+          Locator(
+              "xpath",
+              "/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"),
+          Locator("xpath", "//input[contains(@id,\"search\")]"),
+          Locator("id", "search"),
+          Locator("name", "search_query"),
+          Locator("cssSelector", "#search"),
+      )
+
+  private lateinit var driver: ChromeDriver
+
+  private fun benchmark(by: String, expression: String): Long {
+    println("start $by")
+    val start = Instant.now().toEpochMilli()
+    println("start: $start")
+
+    driver.findElement(commandsByName[by]!!(expression))
+
+    val end = Instant.now().toEpochMilli()
+    println("end: $end")
+    println("delta: ${end - start}")
+    return end - start
+  }
 
   @Test
   fun `Selenium Locator Benchmark`() {
@@ -15,69 +47,30 @@ class SeleniumBenchmark {
     chromeOptions.addArguments("--no-sandbox")
     chromeOptions.addArguments("--headless")
     chromeOptions.addArguments("disable-gpu")
-    val driver = ChromeDriver(chromeOptions)
+    driver = ChromeDriver(chromeOptions)
 
-    driver.get("https://www.youtube.com/")
+    driver.get(target)
 
     Thread.sleep(5000)
-
-    for (i in 1..100) {
-      println("start xpath")
-      val startXpath1 = Instant.now().toEpochMilli()
-      println("start: $startXpath1")
-
-      driver.findElement(By.xpath("//*[@id=\"search\"]"))
-
-      val endXpath1 = Instant.now().toEpochMilli()
-      println("end: $endXpath1")
-      println("delta: ${endXpath1 - startXpath1}")
-
-      println("\nstart full xpath")
-      val startXpath2 = Instant.now().toEpochMilli()
-      println("start: $startXpath2")
-
-      driver.findElement(
-          By.xpath(
-              "/html/body/ytd-app/div[1]/div/ytd-masthead/div[4]/div[2]/ytd-searchbox/form/div[1]/div[1]/input"))
-
-      val endXpath2 = Instant.now().toEpochMilli()
-      println("end: $endXpath2")
-      println("delta: ${endXpath2 - startXpath2}")
-
-      println("\nstart id")
-      val startId = Instant.now().toEpochMilli()
-      println("start: $startId")
-
-      driver.findElement(By.id("search"))
-
-      val endId = Instant.now().toEpochMilli()
-      println("end: $endId")
-      println("delta: ${endId - startId}")
-
-      println("\nstart name")
-      val startName = Instant.now().toEpochMilli()
-      println("start: $startName")
-
-      driver.findElement(By.name("search_query"))
-
-      val endName = Instant.now().toEpochMilli()
-      println("end: $endName")
-      println("delta: ${endName - startName}")
-
-      println("\nstart css selector")
-      val startCss = Instant.now().toEpochMilli()
-      println("start: $startCss")
-
-      driver.findElement(By.cssSelector("#search"))
-
-      val endCss = Instant.now().toEpochMilli()
-      println("end: $endCss")
-      println("delta: ${endCss - startCss}")
-
-      File("results.csv")
-          .appendText(
-              "\n$i,${endXpath1 - startXpath1},${endXpath2 - startXpath2},${endId - startId},${endName - startName},${endCss - startCss}")
+    for (i in 1..testLocators.size) {
+      File("results.csv").appendText("\"${testLocators[i-1].by}\"")
+      if (i != testLocators.size) {
+        File("results.csv").appendText(",")
+      }
     }
+    File("results.csv").appendText("\n")
+
+    for (i in 1..numberOfCycle) {
+      for (j in 1..testLocators.size) {
+        File("results.csv")
+            .appendText("${benchmark(testLocators[j-1].by,testLocators[j-1].expression)}")
+        if (j != testLocators.size) {
+          File("results.csv").appendText(",")
+        }
+      }
+      File("results.csv").appendText("\n")
+    }
+
     driver.quit()
   }
 }
